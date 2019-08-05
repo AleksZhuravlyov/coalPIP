57ff78464229f5ddc0007821b80bfa35c2d69d8c
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2019-08-05 19:54:01 +0500

    coalPIP, inout: replace self with s

--- input/config.py
+++ input/config.py
@@ -8,81 +8,81 @@ sys.path.append(os.path.join(current_path, '../'))
 
 
 class ConfigFrame:
-    def __init__(self):
-        self.__parameters_file = None
-        self.__steady_time_min = None
-        self.__steady_time_max = None
-        self.__transient_time_min = None
-        self.__transient_time_max = None
+    def __init__(s):
+        s.__parameters_file = None
+        s.__steady_time_min = None
+        s.__steady_time_max = None
+        s.__transient_time_min = None
+        s.__transient_time_max = None
 
     @property
-    def parameters_file(self):
-        return self.__parameters_file
+    def parameters_file(s):
+        return s.__parameters_file
 
     @parameters_file.setter
-    def parameters_file(self, parameters_file):
-        self.__parameters_file = str(parameters_file)
+    def parameters_file(s, parameters_file):
+        s.__parameters_file = str(parameters_file)
 
     @property
-    def steady_time_min(self):
-        return self.__steady_time_min
+    def steady_time_min(s):
+        return s.__steady_time_min
 
     @steady_time_min.setter
-    def steady_time_min(self, steady_time_min):
-        self.__steady_time_min = float(steady_time_min)
+    def steady_time_min(s, steady_time_min):
+        s.__steady_time_min = float(steady_time_min)
 
     @property
-    def steady_time_max(self):
-        return self.__steady_time_max
+    def steady_time_max(s):
+        return s.__steady_time_max
 
     @steady_time_max.setter
-    def steady_time_max(self, steady_time_max):
-        self.__steady_time_max = float(steady_time_max)
+    def steady_time_max(s, steady_time_max):
+        s.__steady_time_max = float(steady_time_max)
 
     @property
-    def transient_time_min(self):
-        return self.__transient_time_min
+    def transient_time_min(s):
+        return s.__transient_time_min
 
     @transient_time_min.setter
-    def transient_time_min(self, transient_time_min):
-        self.__transient_time_min = float(transient_time_min)
+    def transient_time_min(s, transient_time_min):
+        s.__transient_time_min = float(transient_time_min)
 
     @property
-    def transient_time_max(self):
-        return self.__transient_time_max
+    def transient_time_max(s):
+        return s.__transient_time_max
 
     @transient_time_max.setter
-    def transient_time_max(self, transient_time_max):
-        self.__transient_time_max = float(transient_time_max)
+    def transient_time_max(s, transient_time_max):
+        s.__transient_time_max = float(transient_time_max)
 
 
 class Config(ConfigFrame):
-    def __init__(self, file_name):
+    def __init__(s, file_name):
         super().__init__()
-        self.parse(file_name)
+        s.parse(file_name)
 
-    def parse(self, file_name):
+    def parse(s, file_name):
         parser = configparser.ConfigParser()
         parser.read(file_name)
-        self.parameters_file = parser.get('Main', 'parameters_file')
-        self.steady_time_min = parser.get('Steady', 'time_min')
-        self.steady_time_max = parser.get('Steady', 'time_max')
-        self.transient_time_min = parser.get('Transient', 'time_min')
-        self.transient_time_max = parser.get('Transient', 'time_max')
+        s.parameters_file = parser.get('Main', 'parameters_file')
+        s.steady_time_min = parser.get('Steady', 'time_min')
+        s.steady_time_max = parser.get('Steady', 'time_max')
+        s.transient_time_min = parser.get('Transient', 'time_min')
+        s.transient_time_max = parser.get('Transient', 'time_max')
 
-    def __str__(self):
+    def __str__(s):
         out_str = '\ncoalPIP config\n'
 
         out_str += '\nMain:\n'
-        out_str += 'parameters_file' + ' = ' + str(self.parameters_file) + '\n'
+        out_str += 'parameters_file' + ' = ' + str(s.parameters_file) + '\n'
 
         out_str += '\nSteady:\n'
-        out_str += 'time_min' + ' = ' + str(self.steady_time_min) + '\n'
-        out_str += 'time_max' + ' = ' + str(self.steady_time_max) + '\n'
+        out_str += 'time_min' + ' = ' + str(s.steady_time_min) + '\n'
+        out_str += 'time_max' + ' = ' + str(s.steady_time_max) + '\n'
 
         out_str += '\nTransient:\n'
-        out_str += 'time_min' + ' = ' + str(self.transient_time_min) + '\n'
-        out_str += 'time_max' + ' = ' + str(self.transient_time_max) + '\n'
+        out_str += 'time_min' + ' = ' + str(s.transient_time_min) + '\n'
+        out_str += 'time_max' + ' = ' + str(s.transient_time_max) + '\n'
 
         return out_str
 
--- input/parameters.py
+++ input/parameters.py
@@ -11,99 +11,99 @@ from input.plot_parameters import plot_parameters
 
 
 class ParametersFrame:
-    def __init__(self):
-        self.__origin = None
-        self.__entire = None
-        self.__steady = None
-        self.__transient = None
+    def __init__(s):
+        s.__origin = None
+        s.__entire = None
+        s.__steady = None
+        s.__transient = None
 
     @property
-    def origin(self):
-        return self.__origin
+    def origin(s):
+        return s.__origin
 
     @origin.setter
-    def origin(self, origin):
-        self.__origin = pd.DataFrame(origin)
+    def origin(s, origin):
+        s.__origin = pd.DataFrame(origin)
 
     @property
-    def entire(self):
-        return self.__entire
+    def entire(s):
+        return s.__entire
 
     @entire.setter
-    def entire(self, entire):
-        self.__entire = pd.DataFrame(entire)
+    def entire(s, entire):
+        s.__entire = pd.DataFrame(entire)
 
     @property
-    def steady(self):
-        return self.__steady
+    def steady(s):
+        return s.__steady
 
     @steady.setter
-    def steady(self, steady):
-        self.__steady = pd.DataFrame(steady)
+    def steady(s, steady):
+        s.__steady = pd.DataFrame(steady)
 
     @property
-    def transient(self):
-        return self.__transient
+    def transient(s):
+        return s.__transient
 
     @transient.setter
-    def transient(self, transient):
-        self.__transient = pd.DataFrame(transient)
+    def transient(s, transient):
+        s.__transient = pd.DataFrame(transient)
 
 
 class Parameters(ParametersFrame):
-    def __init__(self, config_file):
+    def __init__(s, config_file):
         super().__init__()
-        self.__config = Config(file_name=config_file)
+        s.__config = Config(file_name=config_file)
 
     # Process particular parameters types.
 
-    def process_origin(self):
-        self.origin = pd.read_csv(self.__config.parameters_file,
+    def process_origin(s):
+        s.origin = pd.read_csv(s.__config.parameters_file,
                                   index_col='time', parse_dates=True)
-        self.origin.index.name = 'time'
-        self.origin.columns.name = 'parameters'
+        s.origin.index.name = 'time'
+        s.origin.columns.name = 'parameters'
 
-    def process_entire(self):
-        self.process_origin()
-        self.entire = self.origin
-        self.__data_to_seconds(self.entire)
+    def process_entire(s):
+        s.process_origin()
+        s.entire = s.origin
+        s.__data_to_seconds(s.entire)
 
-    def process_steady(self):
-        self.process_origin()
-        self.steady = self.origin
-        self.__data_to_seconds(self.steady)
-        self.steady = self.steady[
-            self.steady.index >= self.__config.steady_time_min]
-        self.steady = self.steady[
-            self.steady.index <= self.__config.steady_time_max]
+    def process_steady(s):
+        s.process_origin()
+        s.steady = s.origin
+        s.__data_to_seconds(s.steady)
+        s.steady = s.steady[
+            s.steady.index >= s.__config.steady_time_min]
+        s.steady = s.steady[
+            s.steady.index <= s.__config.steady_time_max]
 
-    def process_transient(self):
-        self.process_origin()
-        self.transient = self.origin
-        self.__data_to_seconds(self.transient)
-        self.transient = self.transient[
-            self.transient.index >= self.__config.transient_time_min]
-        self.transient = self.transient[
-            self.transient.index <= self.__config.transient_time_max]
+    def process_transient(s):
+        s.process_origin()
+        s.transient = s.origin
+        s.__data_to_seconds(s.transient)
+        s.transient = s.transient[
+            s.transient.index >= s.__config.transient_time_min]
+        s.transient = s.transient[
+            s.transient.index <= s.__config.transient_time_max]
 
-    def __data_to_seconds(self, data_sample):
-        time_series = pd.Series(self.origin.index - self.origin.index[0])
+    def __data_to_seconds(s, data_sample):
+        time_series = pd.Series(s.origin.index - s.origin.index[0])
         data_sample.index = time_series.dt.total_seconds()
         data_sample.index.name = 'time, s'
 
     # Plot particular parameters types.
 
-    def plot_origin(self, y_min=None, y_max=None, y2_min=None, y2_max=None):
-        plot_parameters(self.origin, 'origin', y_min, y_max, y2_min, y2_max)
+    def plot_origin(s, y_min=None, y_max=None, y2_min=None, y2_max=None):
+        plot_parameters(s.origin, 'origin', y_min, y_max, y2_min, y2_max)
 
-    def plot_entire(self, y_min=None, y_max=None, y2_min=None, y2_max=None):
-        plot_parameters(self.entire, 'entire', y_min, y_max, y2_min, y2_max)
+    def plot_entire(s, y_min=None, y_max=None, y2_min=None, y2_max=None):
+        plot_parameters(s.entire, 'entire', y_min, y_max, y2_min, y2_max)
 
-    def plot_steady(self, y_min=None, y_max=None, y2_min=None, y2_max=None):
-        plot_parameters(self.steady, 'steady', y_min, y_max, y2_min, y2_max)
+    def plot_steady(s, y_min=None, y_max=None, y2_min=None, y2_max=None):
+        plot_parameters(s.steady, 'steady', y_min, y_max, y2_min, y2_max)
 
-    def plot_transient(self, y_min=None, y_max=None, y2_min=None, y2_max=None):
-        plot_parameters(self.transient, 'transient', y_min, y_max, y2_min, y2_max)
+    def plot_transient(s, y_min=None, y_max=None, y2_min=None, y2_max=None):
+        plot_parameters(s.transient, 'transient', y_min, y_max, y2_min, y2_max)
 
 
 if __name__ == '__main__':
