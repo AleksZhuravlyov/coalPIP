f26662e582fd84fa17548b9e1660a1b5cd47e3a9
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2019-08-05 19:49:27 +0500

    coalPIP, inout: split class Config on two classes and added min max

--- input/config.py
+++ input/config.py
@@ -7,12 +7,13 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_path, '../'))
 
 
-class Config:
-    def __init__(self, file_name):
-        # parameters_file is path and file name for processed lab data.
+class ConfigFrame:
+    def __init__(self):
         self.__parameters_file = None
-
-        self.parse(file_name)
+        self.__steady_time_min = None
+        self.__steady_time_max = None
+        self.__transient_time_min = None
+        self.__transient_time_max = None
 
     @property
     def parameters_file(self):
@@ -22,17 +23,66 @@ class Config:
     def parameters_file(self, parameters_file):
         self.__parameters_file = str(parameters_file)
 
+    @property
+    def steady_time_min(self):
+        return self.__steady_time_min
+
+    @steady_time_min.setter
+    def steady_time_min(self, steady_time_min):
+        self.__steady_time_min = float(steady_time_min)
+
+    @property
+    def steady_time_max(self):
+        return self.__steady_time_max
+
+    @steady_time_max.setter
+    def steady_time_max(self, steady_time_max):
+        self.__steady_time_max = float(steady_time_max)
+
+    @property
+    def transient_time_min(self):
+        return self.__transient_time_min
+
+    @transient_time_min.setter
+    def transient_time_min(self, transient_time_min):
+        self.__transient_time_min = float(transient_time_min)
+
+    @property
+    def transient_time_max(self):
+        return self.__transient_time_max
+
+    @transient_time_max.setter
+    def transient_time_max(self, transient_time_max):
+        self.__transient_time_max = float(transient_time_max)
+
+
+class Config(ConfigFrame):
+    def __init__(self, file_name):
+        super().__init__()
+        self.parse(file_name)
+
     def parse(self, file_name):
         parser = configparser.ConfigParser()
         parser.read(file_name)
-
-        self.parameters_file = str(parser.get('Main', 'parameters_file'))
+        self.parameters_file = parser.get('Main', 'parameters_file')
+        self.steady_time_min = parser.get('Steady', 'time_min')
+        self.steady_time_max = parser.get('Steady', 'time_max')
+        self.transient_time_min = parser.get('Transient', 'time_min')
+        self.transient_time_max = parser.get('Transient', 'time_max')
 
     def __str__(self):
         out_str = '\ncoalPIP config\n'
 
         out_str += '\nMain:\n'
         out_str += 'parameters_file' + ' = ' + str(self.parameters_file) + '\n'
+
+        out_str += '\nSteady:\n'
+        out_str += 'time_min' + ' = ' + str(self.steady_time_min) + '\n'
+        out_str += 'time_max' + ' = ' + str(self.steady_time_max) + '\n'
+
+        out_str += '\nTransient:\n'
+        out_str += 'time_min' + ' = ' + str(self.transient_time_min) + '\n'
+        out_str += 'time_max' + ' = ' + str(self.transient_time_max) + '\n'
 
         return out_str
 
