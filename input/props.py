a2d51bb37727026b06b2c436d4831739f09b723a
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2019-08-20 13:50:45 +0500

    coalPIP, input: enhanced python class Props

--- input/props.py
+++ input/props.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import configparser
+import numpy as np
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_path, '../'))
@@ -11,60 +12,21 @@ class Props:
         s.__config = configparser.ConfigParser()
         s.__config.read(config_file)
 
-        s.__a_dens = None
-        s.a_dens = s.__config.get('Properties', 'a_dens')
+        s.a_dens = float(s.__config.get('Properties', 'a_dens'))
+        s.b_dens = float(s.__config.get('Properties', 'b_dens'))
+        s.visc = float(s.__config.get('Properties', 'visc'))
+        s.length = float(s.__config.get('Properties', 'length'))
+        s.area = float(s.__config.get('Properties', 'area'))
 
-        s.__b_dens = None
-        s.b_dens = s.__config.get('Properties', 'b_dens')
+    def get_props_array(s):
+        props_list = list()
+        props_list.append(s.a_dens)
+        props_list.append(s.b_dens)
+        props_list.append(s.visc)
+        props_list.append(s.length)
+        props_list.append(s.area)
 
-        s.__visc = None
-        s.visc = s.__config.get('Properties', 'visc')
-
-        s.__length = None
-        s.length = s.__config.get('Properties', 'length')
-
-        s.__area = None
-        s.area = s.__config.get('Properties', 'area')
-
-    @property
-    def a_dens(s):
-        return s.__a_dens
-
-    @a_dens.setter
-    def a_dens(s, a_dens):
-        s.__a_dens = float(a_dens)
-
-    @property
-    def b_dens(s):
-        return s.__b_dens
-
-    @b_dens.setter
-    def b_dens(s, b_dens):
-        s.__b_dens = float(b_dens)
-
-    @property
-    def visc(s):
-        return s.__visc
-
-    @visc.setter
-    def visc(s, visc):
-        s.__visc = float(visc)
-
-    @property
-    def length(s):
-        return s.__length
-
-    @length.setter
-    def length(s, length):
-        s.__length = float(length)
-
-    @property
-    def area(s):
-        return s.__area
-
-    @area.setter
-    def area(s, area):
-        s.__area = float(area)
+        return np.array(props_list, dtype=float)
 
     def __str__(s):
         out_str = 'a_dens ' + str(s.a_dens)
